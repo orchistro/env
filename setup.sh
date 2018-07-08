3251daa54e4eb51f54274b66f6b3740805745a06
USER <user@YoungHwis-MBP13.local>
2018-07-08 19:07:01 +0900

    Updating setup.sh and bashrc for macOS

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@ echo "Building .bashrc and .bash_profile..."
 echo "source ${HOME}/env/bashrc" > ${HOME}/.bashrc
 cp ${HOME}/.bashrc ${HOME}/.bash_profile
 
-echo "Building .vimrc and .gvimrc..."
-echo "source ${HOME}/env/vimrc" > ${HOME}/.vimrc
-echo "source ${HOME}/env/gvimrc" > ${HOME}/.gvimrc
+echo "Building .vimrc..."
+echo "source ${HOME}/env/vimrc" > ${HOME}/.vimrc_shawn
 
 echo "Linking .vim to env/vim..."
 rm -f ${HOME}/.vim
